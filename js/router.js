1df0073a239fc2c1ac2fb354dfeee57fc8abca64
Bob Zurad <bob@zurad.net>
2014-01-08 21:23:50 -0600

    Displaying Model Data

--- js/router.js
+++ js/router.js
@@ -1,3 +1,10 @@
 Todos.Router.map(function() {
     this.resource('todos', { path: '/'});   //render the todos template when path is /
 });
+
+//returns Todo models for Todo template
+Todos.TodosRoute = Ember.Route.extend({
+   model: function() {
+       return this.store.find('todo');
+   }
+});
